38b6d268629f35848759e4590c82081fc0be56f1
RishiSoni252004 <rishisoni2504@gmail.com>
2025-06-13 21:44:05 +0530

    Backend env

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
-import { Link } from "react-router-dom";
-
+import { Link } from "react-router-dom"; // lucide-react
 import AuthImagePattern from "../components/AuthImagePattern";
 import toast from "react-hot-toast";
 
